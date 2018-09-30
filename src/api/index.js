dc92fe5264900b978192975a88ab37a91cde429e
ben-blue <benjaminblue@gmail.com>
2018-09-30 18:13:40 -0500

    Ininital commit

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+class Helper {
+	static baseURL() {
+		return "https://api.foursquare.com/v2";
+	}
+
+	static auth(){
+		const keys = {
+			client_id:"1FBUUYE1FOUN1FZ14UDOSYY2SAV41CPFIIGGVRKUMZJ3BACC",
+			client_secret:"EWZO0YLIZST1NVZOEDIZCECCNWFODUIYVIWXINRDVKJYGAO5",
+			v:"20180930"
+		};
+	}
+}
