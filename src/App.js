f0238585ff35fd05e4fa4ed89cb4b25d48e24945
ben-blue <benjaminblue@gmail.com>
2018-09-30 21:36:43 -0500

    Foursquare search component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SquareAPI from './API/';
 
 class App extends Component {
+  componentDidMount(){
+    SquareAPI.search({
+      near:"Nashville, TN",
+      query: "tacos",
+      limit: 10
+      }).then(results => console.log(results));
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +18,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
