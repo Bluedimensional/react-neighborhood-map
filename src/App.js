cdd6816fc599e036f6a498de9057ad5017fddf47
ben-blue <benjaminblue@gmail.com>
2018-09-30 21:55:30 -0500

    Foursquare search in console and Map on page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SquareAPI from './API/';
+import Map from "./component/Map";
 
 class App extends Component {
   componentDidMount(){
@@ -14,10 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Map/>
       </div>
     );
   }
