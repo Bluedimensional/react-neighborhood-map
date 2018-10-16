66844dd4c4f557ae9becda0fe65edf2f04cef18c
bluedimensional <benjaminblue@gmail.com>
2018-10-16 12:12:23 -0500

    feat: Add responsive image class to footer logos

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,9 +5,9 @@ export default class Footer extends React.Component {
     render() {
       return (
           <Container className="footer"> 
-          <a href="https://www.udacity.com"><img alt="Udacity" src="images/udacity-logo.png"></img></a>
+          <a href="https://www.udacity.com"><img className="img-fluid" alt="Udacity" src="images/udacity-logo.png"></img></a>
             <span>Part of the Front-End Developer Nanodegree</span>
-            <a href="http://reactjs.org"><img alt="React" src="images/react-logo.png"></img></a>    
+            <a href="http://reactjs.org"><img className="img-fluid" alt="React" src="images/react-logo.png"></img></a>    
           </Container>
       );
     }
