bec470d4ab3a31aeeebaf35dec8b1b24f9fa70c9
bluedimensional <benjaminblue@gmail.com>
2018-10-16 10:51:24 -0500

    feat: Alt text for images in footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,9 +5,9 @@ export default class Footer extends React.Component {
     render() {
       return (
           <Container className="footer"> 
-          <a href="https://www.udacity.com"><img src="images/udacity-logo.png"></img></a>
+          <a href="https://www.udacity.com"><img alt="Udacity" src="images/udacity-logo.png"></img></a>
             <span>Part of the Front-End Developer Nanodegree</span>
-            <a href="http://reactjs.org"><img src="images/react-logo.png"></img></a>    
+            <a href="http://reactjs.org"><img alt="React" src="images/react-logo.png"></img></a>    
           </Container>
       );
     }
