32cc93c2f071327039e7f954e81503954994a8d8
bluedimensional <benjaminblue@gmail.com>
2018-10-19 15:35:53 -0500

    Unimplemented components for query quantity

---
+++ src/component/Buttons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+export default class Buttons extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button color="info"></Button>{' '}
+      </div>
+    );
+  }
+}
