9d2fd2b48c96a95033e337680a16d2d88959517a
bluedimensional <benjaminblue@gmail.com>
2018-10-19 17:09:29 -0500

    feat: Footer with logos and text

--- src/component/FooterBlocks.js
+++ src/component/FooterBlocks.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Container, Row, Col, Media } from 'reactstrap';
+import { Col, Media } from 'reactstrap';
 
 export default class FooterBlocks extends React.Component {
   render() {
     return (
-      <Container className="pt-3 pb-3">
-        <Row>
+      <footer className="pt-3 pb-3 row">
+      <div className="col-sm-12 mb-3"><h4 className="text-center secondary-branded">Resources used in this app</h4></div>
           <Col sm={{ size: 4 }}>
             <Media href="https://reactjs.org">
               <Media className="img-fluid" object src="images/React-icon.png" alt="A JavaScript library for building user interfaces" />
@@ -19,13 +19,13 @@ export default class FooterBlocks extends React.Component {
             </Col>
           <Col sm={{ size: 4 }}>
             <Media href="https://developer.foursquare.com">
-              <Media className="img-fluid" object src="images/foursquare-logo.png" alt="Foursqaure Developers" />
+              <Media className="img-fluid d-inline-flex" object src="images/foursquare-logo.png" alt="Foursqaure Developers" />
               </Media>
               <Media body>
                 <Media heading>
                     Foursqaure Developers
                 </Media>  
-              Join over 125,000 developers building location-aware experiences with Foursquare technology and data
+              Places API for location-based experiences
             </Media>
           </Col>
           <Col sm={{ size: 4 }}>
@@ -34,13 +34,12 @@ export default class FooterBlocks extends React.Component {
                 </Media>
                 <Media body>
                   <Media heading>
-                      Foursqaure Developers
+                      Google Maps API
                   </Media>
-                  Build customized, agile experiences that bring the real world to your users with static and dynamic maps,
+                  The power of customized Google Maps
               </Media>
           </Col>
-        </Row>
-      </Container>
+      </footer>
     );
   }
 }
