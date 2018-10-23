54ea32aa80190bdca688b4b911d28713b1da1c89
ben blue <benjaminblue@gmail.com>
2018-10-23 11:51:48 -0500

    feat: Change to simple text line with links to resources

--- src/component/FooterBlocks.js
+++ src/component/FooterBlocks.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Col, Media } from 'reactstrap';
 
 export default class FooterBlocks extends React.Component {
   render() {
     return (
       <footer className="pt-3 pb-3 row">
-      <div className="col-sm-12 mb-3"><h4 className="text-center secondary-branded">Resources used in this app</h4></div>
-          <Col sm={{ size: 4 }}>
+      <div className="col-sm-12 mb-3"><p>Resources used in this app include <a href="https://developer.foursquare.com">Foursqaure API</a> for venue details, <a href="https://developers.google.com/maps/documentation/">Google Maps API</a> for the custom Google Map, and <a href="https://reactjs.org">ReactJS</a> JavaScript library.</p></div>
+          {/* <Col sm={{ size: 4 }}>
             <Media href="https://reactjs.org">
               <Media className="img-fluid" object src="images/React-icon.png" alt="A JavaScript library for building user interfaces" />
               </Media>
@@ -38,7 +37,7 @@ export default class FooterBlocks extends React.Component {
                   </Media>
                   The power of customized Google Maps
               </Media>
-          </Col>
+          </Col> */}
       </footer>
     );
   }
