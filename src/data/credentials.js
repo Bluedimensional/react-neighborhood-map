59ae095534293f306ec0e1e741086030f2663da5
ben blue <benjaminblue@gmail.com>
2018-10-25 11:34:34 -0500

    chore: Removed from repo

--- src/data/credentials.js
+++
@@ -1,8 +0,0 @@
-//  Foursquare API keys:
-export const client_id = '1FBUUYE1FOUN1FZ14UDOSYY2SAV41CPFIIGGVRKUMZJ3BACC';
-export const client_secret = 'EWZO0YLIZST1NVZOEDIZCECCNWFODUIYVIWXINRDVKJYGAO5';
-export const client_version = "20180930";
-
-//   Google Maps API key
-export const key = 'AIzaSyBgtzCcU3cMjtTQJ03SRVa1-CNoPn7HkpA';
-export const url = 'https://maps.googleapis.com/maps/api/js?';
--- src/data/your_credentials.js
+++
@@ -1,8 +0,0 @@
-//  Foursquare API keys:
-export const client_id = 'YOUR_FOURSQAURE_CLIENT_ID';
-export const client_secret = 'YOUR_FOURSQUARE_CLIENT_SECRET';
-export const client_version = "YOUR_FOURSQUARE_VERSION";
-
-//   Google Maps API key
-export const key = 'YOUR_GOOGLE_MAPS_API_KEY';
-export const url = 'https://maps.googleapis.com/maps/api/js?';
