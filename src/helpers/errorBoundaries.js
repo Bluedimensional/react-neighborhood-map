4b13921390a4ecc82fbdfce0aaf58e97a668cc1a
ben blue <benjaminblue@gmail.com>
2018-10-26 16:51:23 -0500

    feat: Style error message to Bootstrap alert

--- src/helpers/errorBoundaries.js
+++ src/helpers/errorBoundaries.js
@@ -1,36 +1,3 @@
-// import React from "react";
-
-// class ErrorBoundary extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = { hasError: false };
-//     }
-  
-//     static getDerivedStateFromError(error) {
-//       // Update state so the next render will show the fallback UI.
-//       return { hasError: true };
-//     }
-  
-//     componentDidCatch(error, info) {
-//       // You can also log the error to an error reporting service
-//       console.log(error, info);
-//     }
-  
-//     render() {
-//       if (this.state.hasError) {
-//         // You can render any custom fallback UI
-//         return <h1>Something went wrong.</h1>;
-//       }
-  
-//       return this.props.children; 
-//     }
-//   }
-
-//   export default ErrorBoundary;
-
-
-
-
 import React from "react"
 class ErrorBoundary extends React.Component {
     constructor(props) {
@@ -51,7 +18,7 @@ class ErrorBoundary extends React.Component {
       if (this.state.errorInfo) {
         // Error path
         return (
-          <div>
+          <div className="alert alert-warning" role="alert">
             <h2>Something went wrong.</h2>
             <details style={{ whiteSpace: 'pre-wrap' }}>
               {this.state.error && this.state.error.toString()}
@@ -67,55 +34,3 @@ class ErrorBoundary extends React.Component {
   }
   
   export default ErrorBoundary  
-
-
-
-
-  // class BuggyCounter extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = { counter: 0 };
-  //     this.handleClick = this.handleClick.bind(this);
-  //   }
-    
-  //   handleClick() {
-  //     this.setState(({counter}) => ({
-  //       counter: counter + 1
-  //     }));
-  //   }
-    
-  //   render() {
-  //     if (this.state.counter === 5) {
-  //       // Simulate a JS error
-  //       throw new Error('I crashed!');
-  //     }
-  //     return <h1 onClick={this.handleClick}>{this.state.counter}</h1>;
-  //   }
-  // }
-  
-  // function App() {
-  //   return (
-  //     <div>
-  //       <p>
-  //         <b>
-  //           This is an example of error boundaries in React 16.
-  //           <br /><br />
-  //           Click on the numbers to increase the counters.
-  //           <br />
-  //           The counter is programmed to throw when it reaches 5. This simulates a JavaScript error in a component.
-  //         </b>
-  //       </p>
-  //       <hr />
-  //       <ErrorBoundary>
-  //         <p>These two counters are inside the same error boundary. If one crashes, the error boundary will replace both of them.</p>
-  //         <BuggyCounter />
-  //         <BuggyCounter />
-  //       </ErrorBoundary>
-  //       <hr />
-  //       <p>These two counters are each inside of their own error boundary. So if one crashes, the other is not affected.</p>
-  //       <ErrorBoundary><BuggyCounter /></ErrorBoundary>
-  //       <ErrorBoundary><BuggyCounter /></ErrorBoundary>
-  //     </div>
-  //   );
-  // }
-  
