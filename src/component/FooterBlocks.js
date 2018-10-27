296f0390be4abdeba80315ad7b3e6c2d9594164c
ben blue <benjaminblue@gmail.com>
2018-10-27 11:16:04 -0500

    chore: Removed used commented code

--- src/component/FooterBlocks.js
+++ src/component/FooterBlocks.js
@@ -4,40 +4,7 @@ export default class FooterBlocks extends React.Component {
   render() {
     return (
       <footer className="pt-3 pb-3 row">
-      <div className="col-sm-12 mb-3"><p>Resources used in this app include <a href="https://developer.foursquare.com">Foursqaure API</a> for venue details, <a href="https://developers.google.com/maps/documentation/">Google Maps API</a> for the custom Google Map, and <a href="https://reactjs.org">ReactJS</a> JavaScript library.</p></div>
-          {/* <Col sm={{ size: 4 }}>
-            <Media href="https://reactjs.org">
-              <Media className="img-fluid" object src="images/React-icon.png" alt="A JavaScript library for building user interfaces" />
-              </Media>
-              <Media body>
-                <Media heading>
-                    React
-                </Media>
-                A JavaScript library for building user interfaces
-              </Media>
-            </Col>
-          <Col sm={{ size: 4 }}>
-            <Media href="https://developer.foursquare.com">
-              <Media className="img-fluid d-inline-flex" object src="images/foursquare-logo.png" alt="Foursqaure Developers" />
-              </Media>
-              <Media body>
-                <Media heading>
-                    Foursqaure Developers
-                </Media>  
-              Places API for location-based experiences
-            </Media>
-          </Col>
-          <Col sm={{ size: 4 }}>
-            <Media className="img-fluid" href="https://developers.google.com/maps/documentation/">
-                <Media object src="images/Google-maps-logo.png" alt="Google Maps API" />
-                </Media>
-                <Media body>
-                  <Media heading>
-                      Google Maps API
-                  </Media>
-                  The power of customized Google Maps
-              </Media>
-          </Col> */}
+        <div className="col-sm-12 mb-3"><p>Resources used in this app include <a href="https://developer.foursquare.com">Foursqaure API</a> for venue details, <a href="https://developers.google.com/maps/documentation/">Google Maps API</a> for the custom Google Map, and <a href="https://reactjs.org">ReactJS</a> JavaScript library.</p></div>
       </footer>
     );
   }
