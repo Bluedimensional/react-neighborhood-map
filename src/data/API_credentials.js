4f7dbed24bd499069182dbe66b1ef269f3831510
ben blue <benjaminblue@gmail.com>
2018-10-28 11:31:30 -0500

    chore: Remove credentials from ignore for submission

---
+++ src/data/API_credentials.js
@@ -0,0 +1,8 @@
+//  Foursquare API
+export const client_id = '1FBUUYE1FOUN1FZ14UDOSYY2SAV41CPFIIGGVRKUMZJ3BACC';
+export const client_secret = 'EWZO0YLIZST1NVZOEDIZCECCNWFODUIYVIWXINRDVKJYGAO5';
+export const client_version = "20180930";
+
+//   Google Maps API
+export const key = 'AIzaSyBgtzCcU3cMjtTQJ03SRVa1-CNoPn7HkpA';
+export const url = 'https://maps.googleapis.com/maps/api/js?';
