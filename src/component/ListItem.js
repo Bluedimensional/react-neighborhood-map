442c48296e5d081367585aad294ee88dc8f423c6
ben blue <benjaminblue@gmail.com>
2018-10-28 12:17:54 -0500

    feat: Add role of listitem to li

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+// Great work adding tabIndex = 0 to the items in the side menu but you need to add onKeyPress event listener to be able to use the keyboard and choose an item in the list
+
 export default class ListItem extends Component {
 	render() {
 		return(
-			<li tabIndex="0" className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
+			<li role="listitem="0" className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
 			<img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name} />
 				{this.props.name}
 				{/* {this.props.location['address']}} */}
