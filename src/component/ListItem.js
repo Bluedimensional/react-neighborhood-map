996068768ba0eb56ed903bbfbbddb1f5fecdcabe
ben blue <benjaminblue@gmail.com>
2018-10-29 13:02:59 -0500

    chore: Typo fix

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 export default class ListItem extends Component {
 	render() {
 		return(
-			<li role="listitem="0" className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
+			<li role="listitem" className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
 			<img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name} />
 				{this.props.name}
 				{/* {this.props.location['address']}} */}
